a9f3fb313f684788ca79d27b16b91c4cac58ad1d
heardit <heardit@gmail.com>
2012-03-15 12:04:02 -0500

    adding to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fattime::Application.config.secret_token = 'dcc09c34f3f8ba388ad97de3964708e916a31b5537bcc0f346b526f16d8f2aa916b8b33b489027c7c2a10b3773fae37e6778bb131602c709c7d60db1e1c55d44'
